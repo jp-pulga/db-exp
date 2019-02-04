ad092ee6558ed0d8acfa8f622ae291aada8b8b75
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-02-04 11:34:01 -0200

    Fixed all warnings and build errors.

--- src/db.rs
+++ src/db.rs
@@ -1,17 +1,17 @@
-use actix::{Actor, Addr, SyncContext, SyncArbiter};
+use actix::{Actor, Addr, SyncArbiter, SyncContext};
 use num_cpus;
-use r2d2;
-use r2d2_sqlite;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 
-pub struct DBExecutor(pub r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>);
+pub struct DBExecutor(pub Pool<SqliteConnectionManager>);
 
 impl Actor for DBExecutor {
 	type Context = SyncContext<Self>;
 }
 
 pub fn initialize_db_exp_connection() -> Addr<DBExecutor> {
-	let manager = r2d2_sqlite::SqliteConnectionManager::file("weather.db");
-	let pool = r2d2::Pool::<r2d2_sqlite::SqliteConnectionManager>::new(manager).unwrap();
+	let manager = SqliteConnectionManager::file("db-exp.db");
+	let pool = Pool::<SqliteConnectionManager>::new(manager).unwrap();
 
 	SyncArbiter::start(num_cpus::get(), move || DBExecutor(pool.clone()))
 }
--- src/handlers/dashboard.rs
+++ src/handlers/dashboard.rs
@@ -1,9 +1,10 @@
 use crate::db::DBExecutor;
 use crate::AppState;
 use actix::{Handler, Message};
-use actix_web::{Error, HttpResponse, State};
+use actix_web::{AsyncResponder, Error, HttpResponse, State};
 use askama::Template;
-use rusqlite::{Connection, NO_PARAMS};
+use futures::future::Future;
+use rusqlite::NO_PARAMS;
 
 /// Dashboard specific information
 /// THis store cards info
@@ -13,7 +14,7 @@ pub struct Dashboard {
 	databases: Vec<String>,
 }
 
-pub enum DashboardQuerys { }
+struct DashboardQuerys {}
 
 impl Message for DashboardQuerys {
 	type Result = Result<Dashboard, Error>;
@@ -22,26 +23,32 @@ impl Message for DashboardQuerys {
 impl Handler<DashboardQuerys> for DBExecutor {
 	type Result = Result<Dashboard, Error>;
 
-	fn handle(&mut self, msg: DashboardQuerys, _: &mut Self::Context) -> Self::Result {
-		let conn: r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager> = self.0.get().unwrap();
-		let r = conn.query_row("select 1", NO_PARAMS, |r| r.get(0)).unwrap();
+	fn handle(&mut self, _msg: DashboardQuerys, _: &mut Self::Context) -> Self::Result {
+		let conn: r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager> =
+			self.0.get().unwrap();
+		let result = conn
+			.query_row("select name from databases", NO_PARAMS, |r| r.get(0))
+			.unwrap();
 
-		Ok(
-			Dashboard {
-				databases: vec![r]
-			}
-		)
+		println!("{}", result);
+
+		Ok(Dashboard {
+			databases: vec![result],
+		})
 	}
 }
 
 /// Show the dashboard
-pub fn show_dashboard(state: State<AppState>) -> Result<HttpResponse, Error> {
-	let result = state.db.query_row("select 'DataBase Name'", NO_PARAMS, |r| r.get(0)).unwrap();
-
-	let s = Dashboard {
-		databases: vec!["DataBase 1".to_string(), "DataBase 2".to_string()],
-	}
-	.render()
-	.unwrap();
-	Ok(HttpResponse::Ok().content_type("text/html").body(s))
+pub fn show_dashboard(state: State<AppState>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+	state
+		.db
+		.send(DashboardQuerys {})
+		.from_err()
+		.and_then(|res| match res {
+			Ok(v) => Ok(HttpResponse::Ok()
+				.content_type("text/html")
+				.body(v.render().unwrap())),
+			Err(_) => Ok(HttpResponse::InternalServerError().into()),
+		})
+		.responder()
 }
